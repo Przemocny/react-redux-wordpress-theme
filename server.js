89c7a60091a5ba35ec2e87b0739b2ae35a0ee4c1
Marek Sieńko <mrszegerege@gmail.com>
2020-02-11 16:34:20 +0100

    server fix

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
-
-app.use('/static', express.static(path.join(__dirname, 'src')));
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/', function (req, res) {
-  console.log('app')
-  res.sendFile(path.join(__dirname, 'src', 'App.js'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
+app.listen(process.env.PORT || 3000);
