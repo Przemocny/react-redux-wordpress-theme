753c5a42e26e13a6a98f7fd34eb6050639a6f126
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-02-19 20:17:09 +0100

    cleaning for prod

--- server.js
+++ server.js
@@ -10,5 +10,7 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log('React app served')
+});
 
