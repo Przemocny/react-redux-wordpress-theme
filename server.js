c27a0573e1b757b443c81a2aa9ab2caae3090a1e
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2020-02-19 20:13:32 +0100

    prepare for build

--- server.js
+++ server.js
@@ -2,13 +2,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/static',express.static(path.join(__dirname, 'src')));
 
-
-
-
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
